52824f64749e9015435dd49eaa23ec9568f7c854
Adrian <volkan.erdogan@abcvyz.com>
2022-02-18 22:28:51 +0300

    SECTION 50 UIKIT CSS FRAMEWORK INSTALLATION     *https://getuikit.com/ CONTROLLERS
    VIEWS
    
    MODELS
    
    ENTITIES
    
    ROUTERS
    
    CONFIG
    
    DATABASE
    
    HELPER
    
    LIBRARY
    
    FILTERS
    
    LANGUAGE

--- app/Views/admin/theme/style.php
+++ app/Views/admin/theme/style.php
@@ -1,1 +1,4 @@
-<link rel="stylesheet" href="<?= base_url('public/admin/css/theme.css'); ?>">
+<link rel="stylesheet" href="<?= base_url(PUBLIC_ADMIN_CSS_PATH . 'theme.css'); ?>">
+<link rel="stylesheet" href="<?= base_url(PUBLIC_ADMIN_CSS_PATH . 'uikit.min.css'); ?>">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
--- themes/default/index.php
+++ themes/default/index.php
@@ -3,6 +3,11 @@
     <title><?= cve_title(); ?></title>
 </head>
 <body>
-<p>Merhaba Dünya!</p>
+
+<div uk-lightbox>
+    <a class="uk-button uk-button-default" href="http://localhost/public/admin/img/avatar/avatar-3.png">Open Lightbox</a>
+</div>
+
+
 </body>
 </html>
